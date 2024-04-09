3e4e273243f1ecbd7fe5c196951f65a4c31ece37
Lukasz Gryglicki <lgryglicki@cncf.io>
2024-04-09 08:40:40 +0200

    handle debian 12 rv64
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@
 # cc -static -o c c.s
 cc -static -nostdlib -nostartfiles -o c c.s && strip -s c && brandelf -t freebsd c && ./c && echo ok
 as -o a.o c.s && ld -o a a.o && strip -s a && brandelf -t freebsd a && ./a && echo ok
+as -o hello.o hello.s && ld -o hello hello.o && strip -s hello && ./hello && echo 'ok'
